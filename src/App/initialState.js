62606dc6820bddb697afe29943a54630c712f87e
Christian <ChrisWcs@users.noreply.github.com>
2017-08-30 12:46:03 -0600

    Add mainReducer tests, all are passing

--- src/App/initialState.js
+++ src/App/initialState.js
@@ -2,10 +2,7 @@ const initialState = () => ({
     m: "",
     b: "",
     eqs: [
-        {
-            m: 3,
-            b: 1,
-        }
+
     ],
 });
 
--- src/app/mainReducer.spec.js
+++ src/app/mainReducer.spec.js
@@ -1,1 +1,48 @@
+import { createAddEq, createChangeB, createChangeM } from './lines/actionCreator';
+
 import mainReducer from './mainReducer';
+import initialState from './initialState';
+
+describe('mainReducer tests', () => {
+
+  it('changing state.b', () => {
+
+    let expected = initialState();
+    expected.b = "hello";
+
+    let actual = mainReducer(initialState(), createChangeB("hello"));
+
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('changing state.m', () => {
+    
+    let expected = initialState();
+    expected.m = "hello";
+
+    let actual = mainReducer(initialState(), createChangeM("hello"));
+
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('testing adding Eq', () => {
+    
+    let expected = initialState();
+    expected.m = "5";
+    expected.b = "3";
+    expected.eqs = [
+        {
+            m: 5,
+            b: 3,
+        }
+    ];
+
+    let actual = mainReducer(initialState(), createChangeM("5"));
+    actual = mainReducer( actual, createChangeB("3"));
+    actual = mainReducer( actual, createAddEq());
+
+    expect(actual).toEqual(expected);
+  });
+});
